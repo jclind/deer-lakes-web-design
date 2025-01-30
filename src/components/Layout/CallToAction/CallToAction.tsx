1ab925ae75e8d2c7b1514f769695e3d5f3d8d77c
Jesse Lind <52766281+jclind@users.noreply.github.com>
2025-01-30 17:47:24 -0500

    build: add CallToAction component and update pricing layout to be space-between on largest media size

---
+++ src/components/Layout/CallToAction/CallToAction.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import styles from './CallToAction.module.scss'
+import { ButtonLink } from '../../ButtonLink'
+const CallToAction = () => {
+  return (
+    <div className={styles.call_to_action}>
+      <div className={`${styles.card} card page__inner`}>
+        <div className={styles.image}>
+          <img src='/images/DLWD_icon.png' alt='Deer Lakes Web Design Icon' />
+        </div>
+        <div className={styles.content}>
+          <h1 className='section-title'>Ready To Get Started?</h1>
+          <p className='section-paragraph'>
+            Let’s make your business stand out online. Get in touch with us
+            today to discuss your project, and we’ll help you get started with a
+            website that works for you.
+          </p>
+          <ButtonLink text='Contact Us' link='/contact' size='large' />
+        </div>
+      </div>
+      <div className={styles.half_background}></div>
+    </div>
+  )
+}
+
+export default CallToAction
