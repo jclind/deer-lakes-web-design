ba5a8d1f24fb17b3849ef2b9310ad642dbc2be57
Jesse Lind <52766281+jclind@users.noreply.github.com>
2025-02-03 18:45:52 -0500

    feat(MountainsHero): add dark mode and children passthrough to the MountainsHero component and add hero_mountains_dark.svg image

--- src/components/Common/MountainsHero/MountainsHero.tsx
+++ src/components/Common/MountainsHero/MountainsHero.tsx
@@ -1,10 +1,23 @@
 import React from 'react'
 import styles from './MountainsHero.module.scss'
-const MountainsHero = ({ title }: { title: string }) => {
+const MountainsHero = ({
+  title,
+  isDark = false,
+  children,
+}: {
+  title: string
+  isDark?: boolean
+  children?: React.ReactNode
+}) => {
   return (
-    <div className={styles.mountains_hero}>
+    <div className={`${styles.mountains_hero} ${isDark ? styles.dark : ''}`}>
       <div className='page-title'>{title}</div>
-      <div className={styles.mountains_image}></div>
+      {children}
+      {isDark ? (
+        <div className={styles.mountains_image_dark}></div>
+      ) : (
+        <div className={styles.mountains_image_light}></div>
+      )}
     </div>
   )
 }
