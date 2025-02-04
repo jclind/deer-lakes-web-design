e319ddd42bc488ebaf553840b0d4a68194123ce6
Jesse Lind <52766281+jclind@users.noreply.github.com>
2025-02-04 16:53:59 -0500

    feat(Terms|Privacy): add Terms Of Service page along with content

---
+++ src/components/Pages/TermsAndPrivacy/PrivacyPolicy.tsx
@@ -0,0 +1,377 @@
+import React from 'react'
+import styles from './TermsAndPrivacy.module.scss'
+const PrivacyPolicy = () => {
+  return (
+    <div className={styles.privacy_policy}>
+      <div className='page__inner'>
+        <div className={styles.content}>
+          <h1 className='page-title'>PRIVACY POLICY</h1>
+          <div className={styles.last_updated}>
+            <strong>Last updated February 04, 2025</strong>
+          </div>
+
+          <p>
+            This Privacy Notice for <strong>Deer Lakes Web Design</strong>{' '}
+            ("we," "us," or "our"), describes how and why we might access,
+            collect, store, use, and/or share ("process") your personal
+            information when you use our services ("Services"), including when
+            you:
+          </p>
+          <ul>
+            <li>
+              Visit our website at{' '}
+              <a
+                href='https://deerlakeswebdesigns.netlify.app'
+                target='_blank'
+                rel='noopener noreferrer'
+              >
+                https://deerlakeswebdesigns.netlify.app
+              </a>
+              , or any website of ours that links to this Privacy Notice.
+            </li>
+            <li>
+              Engage with us in other related ways, including any sales,
+              marketing, or events.
+            </li>
+          </ul>
+
+          <p>
+            <strong>Questions or concerns?</strong> Reading this Privacy Notice
+            will help you understand your privacy rights and choices. If you do
+            not agree with our policies and practices, please do not use our
+            Services. If you still have any questions or concerns, please
+            contact us at{' '}
+            <a href='mailto:jesselindcs@gmail.com'>jesselindcs@gmail.com</a>.
+          </p>
+
+          <h2 className='section-title'>SUMMARY OF KEY POINTS</h2>
+          <p>
+            <em>
+              This summary provides key points from our Privacy Notice, but you
+              can find out more details about any of these topics by clicking
+              the link following each key point or by using our table of
+              contents below to find the section you are looking for.
+            </em>
+          </p>
+          <p>
+            <strong>What personal information do we process?</strong> When you
+            visit, use, or navigate our Services, we may process personal
+            information depending on how you interact with us and the Services,
+            the choices you make, and the products and features you use.
+          </p>
+          <p>
+            <strong>Do we process any sensitive personal information?</strong>{' '}
+            We do not process sensitive personal information.
+          </p>
+          <p>
+            <strong>Do we collect any information from third parties?</strong>{' '}
+            We do not collect any information from third parties.
+          </p>
+          <p>
+            <strong>How do we process your information?</strong> We process your
+            information to provide, improve, and administer our Services,
+            communicate with you, for security and fraud prevention, and to
+            comply with law.
+          </p>
+          <p>
+            <strong>
+              In what situations and with which parties do we share personal
+              information?
+            </strong>{' '}
+            We may share information in specific situations and with specific
+            third parties.
+          </p>
+          <p>
+            <strong>How do we keep your information safe?</strong> We have
+            organizational and technical processes and procedures in place to
+            protect your personal information.
+          </p>
+          <p>
+            <strong>What are your rights?</strong> Depending on where you are
+            located geographically, the applicable privacy law may mean you have
+            certain rights regarding your personal information.
+          </p>
+          <p>
+            <strong>How do you exercise your rights?</strong> The easiest way to
+            exercise your rights is by submitting a data subject access request
+            or by contacting us.
+          </p>
+
+          <h2 className='section-title'>TABLE OF CONTENTS</h2>
+          <ol>
+            <li>
+              <a href='#infocollect'>WHAT INFORMATION DO WE COLLECT?</a>
+            </li>
+            <li>
+              <a href='#infouse'>HOW DO WE PROCESS YOUR INFORMATION?</a>
+            </li>
+            <li>
+              <a href='#whoshare'>
+                WHEN AND WITH WHOM DO WE SHARE YOUR PERSONAL INFORMATION?
+              </a>
+            </li>
+            <li>
+              <a href='#cookies'>
+                DO WE USE COOKIES AND OTHER TRACKING TECHNOLOGIES?
+              </a>
+            </li>
+            <li>
+              <a href='#inforetain'>HOW LONG DO WE KEEP YOUR INFORMATION?</a>
+            </li>
+            <li>
+              <a href='#infosafe'>HOW DO WE KEEP YOUR INFORMATION SAFE?</a>
+            </li>
+            <li>
+              <a href='#infominors'>DO WE COLLECT INFORMATION FROM MINORS?</a>
+            </li>
+            <li>
+              <a href='#privacyrights'>WHAT ARE YOUR PRIVACY RIGHTS?</a>
+            </li>
+            <li>
+              <a href='#DNT'>CONTROLS FOR DO-NOT-TRACK FEATURES</a>
+            </li>
+            <li>
+              <a href='#uslaws'>
+                DO UNITED STATES RESIDENTS HAVE SPECIFIC PRIVACY RIGHTS?
+              </a>
+            </li>
+            <li>
+              <a href='#policyupdates'>DO WE MAKE UPDATES TO THIS NOTICE?</a>
+            </li>
+            <li>
+              <a href='#contact'>HOW CAN YOU CONTACT US ABOUT THIS NOTICE?</a>
+            </li>
+            <li>
+              <a href='#request'>
+                HOW CAN YOU REVIEW, UPDATE, OR DELETE THE DATA WE COLLECT FROM
+                YOU?
+              </a>
+            </li>
+          </ol>
+
+          <h2 id='infocollect' className='section-title'>
+            1. WHAT INFORMATION DO WE COLLECT?
+          </h2>
+          <h3>Personal information you disclose to us</h3>
+          <p>
+            <em>In Short:</em> We collect personal information that you provide
+            to us.
+          </p>
+          <p>
+            We collect personal information that you voluntarily provide to us
+            when you express an interest in obtaining information about us or
+            our products and Services, when you participate in activities on the
+            Services, or otherwise when you contact us.
+          </p>
+          <p>
+            <strong>Personal Information Provided by You.</strong> The personal
+            information that we collect depends on the context of your
+            interactions with us and the Services, the choices you make, and the
+            products and features you use. The personal information we collect
+            may include the following:
+          </p>
+          <ul>
+            <li>Names</li>
+            <li>Phone numbers</li>
+            <li>Email addresses</li>
+          </ul>
+          <p>
+            <strong>Sensitive Information.</strong> We do not process sensitive
+            information.
+          </p>
+
+          <h3>Information automatically collected</h3>
+          <p>
+            <em>In Short:</em> Some information — such as your Internet Protocol
+            (IP) address and/or browser and device characteristics — is
+            collected automatically when you visit our Services.
+          </p>
+          <p>
+            We automatically collect certain information when you visit, use, or
+            navigate the Services. This information does not reveal your
+            specific identity but may include device and usage information, such
+            as your IP address, browser and device characteristics, operating
+            system, language preferences, referring URLs, device name, country,
+            location, information about how and when you use our Services, and
+            other technical information.
+          </p>
+
+          <h2 id='infouse' className='section-title'>
+            2. HOW DO WE PROCESS YOUR INFORMATION?
+          </h2>
+          <p>
+            <em>In Short:</em> We process your information to provide, improve,
+            and administer our Services, communicate with you, for security and
+            fraud prevention, and to comply with law.
+          </p>
+          <p>
+            We process your personal information for a variety of reasons,
+            depending on how you interact with our Services, including:
+          </p>
+          <ul>
+            <li>To respond to user inquiries/offer support to users.</li>
+            <li>To send administrative information to you.</li>
+            <li>To fulfill and manage your orders.</li>
+            <li>To post testimonials.</li>
+          </ul>
+
+          <h2 id='whoshare' className='section-title'>
+            3. WHEN AND WITH WHOM DO WE SHARE YOUR PERSONAL INFORMATION?
+          </h2>
+          <p>
+            <em>In Short:</em> We may share information in specific situations
+            described in this section and/or with the following third parties.
+          </p>
+          <p>
+            We may need to share your personal information in the following
+            situations:
+          </p>
+          <ul>
+            <li>
+              <strong>Business Transfers.</strong> We may share or transfer your
+              information in connection with, or during negotiations of, any
+              merger, sale of company assets, financing, or acquisition of all
+              or a portion of our business to another company.
+            </li>
+          </ul>
+
+          <h2 id='cookies' className='section-title'>
+            4. DO WE USE COOKIES AND OTHER TRACKING TECHNOLOGIES?
+          </h2>
+          <p>
+            <em>In Short:</em> We may use cookies and other tracking
+            technologies to collect and store your information.
+          </p>
+          <p>
+            We may use cookies and similar tracking technologies (like web
+            beacons and pixels) to gather information when you interact with our
+            Services. Some online tracking technologies help us maintain the
+            security of our Services, prevent crashes, fix bugs, save your
+            preferences, and assist with basic site functions.
+          </p>
+
+          <h2 id='inforetain' className='section-title'>
+            5. HOW LONG DO WE KEEP YOUR INFORMATION?
+          </h2>
+          <p>
+            <em>In Short:</em> We keep your information for as long as necessary
+            to fulfill the purposes outlined in this Privacy Notice unless
+            otherwise required by law.
+          </p>
+          <p>
+            We will only keep your personal information for as long as it is
+            necessary for the purposes set out in this Privacy Notice, unless a
+            longer retention period is required or permitted by law.
+          </p>
+
+          <h2 id='infosafe' className='section-title'>
+            6. HOW DO WE KEEP YOUR INFORMATION SAFE?
+          </h2>
+          <p>
+            <em>In Short:</em> We aim to protect your personal information
+            through a system of organizational and technical security measures.
+          </p>
+          <p>
+            We have implemented appropriate and reasonable technical and
+            organizational security measures designed to protect the security of
+            any personal information we process.
+          </p>
+
+          <h2 id='infominors' className='section-title'>
+            7. DO WE COLLECT INFORMATION FROM MINORS?
+          </h2>
+          <p>
+            <em>In Short:</em> We do not knowingly collect data from or market
+            to children under 18 years of age.
+          </p>
+          <p>
+            We do not knowingly collect, solicit data from, or market to
+            children under 18 years of age. By using the Services, you represent
+            that you are at least 18 or that you are the parent or guardian of
+            such a minor and consent to such minor dependent’s use of the
+            Services.
+          </p>
+
+          <h2 id='privacyrights' className='section-title'>
+            8. WHAT ARE YOUR PRIVACY RIGHTS?
+          </h2>
+          <p>
+            <em>In Short:</em> You may review, change, or terminate your account
+            at any time, depending on your country, province, or state of
+            residence.
+          </p>
+          <p>
+            <strong>Withdrawing your consent:</strong> If we are relying on your
+            consent to process your personal information, you have the right to
+            withdraw your consent at any time.
+          </p>
+
+          <h2 id='DNT' className='section-title'>
+            9. CONTROLS FOR DO-NOT-TRACK FEATURES
+          </h2>
+          <p>
+            Most web browsers and some mobile operating systems and mobile
+            applications include a Do-Not-Track ("DNT") feature or setting you
+            can activate to signal your privacy preference not to have data
+            about your online browsing activities monitored and collected.
+          </p>
+
+          <h2 id='uslaws' className='section-title'>
+            10. DO UNITED STATES RESIDENTS HAVE SPECIFIC PRIVACY RIGHTS?
+          </h2>
+          <p>
+            <em>In Short:</em> If you are a resident of certain US states, you
+            may have the right to request access to and receive details about
+            the personal information we maintain about you and how we have
+            processed it.
+          </p>
+
+          <h2 id='policyupdates' className='section-title'>
+            11. DO WE MAKE UPDATES TO THIS NOTICE?
+          </h2>
+          <p>
+            <em>In Short:</em> Yes, we will update this notice as necessary to
+            stay compliant with relevant laws.
+          </p>
+          <p>
+            We may update this Privacy Notice from time to time. The updated
+            version will be indicated by an updated "Revised" date at the top of
+            this Privacy Notice.
+          </p>
+
+          <h2 id='contact' className='section-title'>
+            12. HOW CAN YOU CONTACT US ABOUT THIS NOTICE?
+          </h2>
+          <p>
+            If you have questions or comments about this notice, you may email
+            us at{' '}
+            <a href='mailto:jesselindcs@gmail.com'>jesselindcs@gmail.com</a> or
+            contact us by post at:
+          </p>
+          <p>
+            Deer Lakes Web Design
+            <br />
+            106 Oakwood Cir
+            <br />
+            Gibsonia, PA 15044
+            <br />
+            United States
+          </p>
+
+          <h2 id='request' className='section-title'>
+            13. HOW CAN YOU REVIEW, UPDATE, OR DELETE THE DATA WE COLLECT FROM
+            YOU?
+          </h2>
+          <p>
+            Based on the applicable laws of your country or state of residence
+            in the US, you may have the right to request access to the personal
+            information we collect from you, details about how we have processed
+            it, correct inaccuracies, or delete your personal information.
+          </p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default PrivacyPolicy
