68ddff1272cb8b8e3659d1a4047897d24ddb399e
Jesse Lind <jesseclind@gmail.com>
2025-12-04 16:57:31 -0500

    Add porfolio / projects showcase section on home page

--- src/components/Common/ButtonLink/ButtonLink.tsx
+++ src/components/Common/ButtonLink/ButtonLink.tsx
@@ -9,13 +9,18 @@ const ButtonLink = ({
 }: {
   text: string
   link: string
+  isExternalLink?: boolean
   color?: 'primary' | 'secondary' | 'tertiary' | 'white'
   size?: 'small' | 'medium' | 'large' | 'full_width'
 }) => {
+  let isExternalLink = !!link.startsWith('http')
+
   return (
     <a
       href={link}
       className={`${styles.button_link} ${styles[color]} ${styles[size]} button`}
+      target={isExternalLink ? '_blank' : undefined}
+      rel={isExternalLink ? 'noopener noreferrer' : undefined}
     >
       {text}
     </a>
